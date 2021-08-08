4e386b7dd9e27e248408615dfedc294ccb4d6df9
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-08-08 16:51:48 +0300

    Add react-query

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,21 @@ import Login from "./components/pages/Auth/Login";
 import Register from "./components/pages/Auth/Register";
 import "semantic-ui-css/semantic.min.css";
 import Products from "./components/pages/Products";
+import { QueryClient, QueryClientProvider } from "react-query";
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/Products" exact component={Products} />
-        <Route path="/Login" component={Login} />
-        <Route path="/Register" component={Register} />
-      </Switch>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Products" exact component={Products} />
+          <Route path="/Login" component={Login} />
+          <Route path="/Register" component={Register} />
+        </Switch>
+      </Router>
+    </QueryClientProvider>
   );
 }
 function Home() {
