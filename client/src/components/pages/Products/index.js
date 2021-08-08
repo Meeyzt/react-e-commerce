123feca0c12c6117718146c939ea913516813a97
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-08-08 16:52:04 +0300

    List products to react-query

--- client/src/components/pages/Products/index.js
+++ client/src/components/pages/Products/index.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { Grid, GridRow } from "semantic-ui-react";
+import { Grid, GridRow, Placeholder } from "semantic-ui-react";
+import { useQuery } from "react-query";
 import Item from "../../Item";
 
 function Products() {
+  const { isLoading, error, data } = useQuery("Products", () =>
+    fetch("https://fakestoreapi.com/products").then((res) => res.json())
+  );
+  if (isLoading) return "Loading...";
+  if (error) return "An error has occurred:" + error.message;
   return (
     <>
       <h1>Products</h1>
       <Grid doubling relaxed columns={4} padded>
         <GridRow>
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
+          {data &&
+            data.map((data, key) => {
+              return <Item key={key} data={data} />;
+            })}
         </GridRow>
       </Grid>
     </>
