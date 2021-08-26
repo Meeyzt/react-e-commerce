80d565c82145226d78ae2f02682f08a8320073d9
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-08-26 13:25:32 +0300

    Create Private Route

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,11 @@ import Register from "./components/pages/Auth/Register";
 import Loading from "./components/Loading";
 import "semantic-ui-css/semantic.min.css";
 import Products from "./components/pages/Products";
+import Profile from "./components/pages/Profile";
 import { QueryClient, QueryClientProvider } from "react-query";
+import ListUsers from "./components/pages/Admin/ListUsers";
+import LoginRoute from "./components/PrivateRoute/LoginRoute";
+import AdminRoute from "./components/PrivateRoute/AdminRoute";
 
 function App() {
   const queryClient = new QueryClient();
@@ -22,10 +26,16 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <LoginRoute path="/Profile">
+            <Profile />
+          </LoginRoute>
           <Route path="/Products" exact component={Products} />
           <Route path="/Login" component={Login} />
           <Route path="/Register" component={Register} />
           <Route path="/Loading" component={Loading} />
+          <AdminRoute path="/Admin/ListUsers">
+            <ListUsers />
+          </AdminRoute>
         </Switch>
       </Router>
     </QueryClientProvider>
