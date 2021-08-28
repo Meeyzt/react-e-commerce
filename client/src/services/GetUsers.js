51af24e5c82416374771eb23e6115d54ca34a443
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-08-28 17:03:49 +0300

    GetUsers to orderby role

--- client/src/services/GetUsers.js
+++ client/src/services/GetUsers.js
@@ -5,21 +5,22 @@ const GetUsers = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    db.collection("Users").onSnapshot(
-      async (snapshot) => {
-        await setUsers(
-          snapshot.docs.map((t) => ({
-            id: t.id,
-            role: t.data().role,
-            username: t.data().username,
-            profilePhoto: t.data().profilePhoto,
-          }))
-        );
-        console.log(users);
-      },
-      (error) => console.log(error),
-      (onCompletion) => console.log(onCompletion)
-    );
+    db.collection("Users")
+      .orderBy("role")
+      .onSnapshot(
+        async (snapshot) => {
+          await setUsers(
+            snapshot.docs.map((t) => ({
+              id: t.id,
+              role: t.data().role,
+              username: t.data().username,
+              profilePhoto: t.data().profilePhoto,
+            }))
+          );
+        },
+        (error) => console.log(error),
+        (onCompletion) => console.log(onCompletion)
+      );
   }, []);
   return { users };
 };
