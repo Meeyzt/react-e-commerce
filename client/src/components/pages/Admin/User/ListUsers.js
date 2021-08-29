1a47f2c4ffb8f7df3d7e6db0413e70200cf3e744
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-08-29 06:24:03 -0700

    Add Admin Area Previous Page Button

--- client/src/components/pages/Admin/User/ListUsers.js
+++ client/src/components/pages/Admin/User/ListUsers.js
@@ -1,17 +1,20 @@
-import { Segment, List } from "semantic-ui-react";
+import { Segment, List, Button } from "semantic-ui-react";
 import ListUser from "../../../ListUser";
 import GetUsers from "../../../../services/GetUsers";
+import { Link } from "react-router-dom";
 
 function ListUsers() {
   const { users } = GetUsers();
   return (
+    <>  
     <Segment inverted>
-      <List id="adminList" divided inverted relaxed verticalAlign="middle">
-        {users.map((data, i) => {
-          return <ListUser data={data} />;
-        })}
-      </List>
-    </Segment>
+    <Link to="/admin"><Button color="grey" icon="angle left" content="Previous Page"/></Link>
+    <List id="adminList" divided inverted relaxed verticalAlign="middle">
+      {users.map((data, i) => {
+        return <ListUser data={data} />;
+      })}
+    </List>
+  </Segment></>
   );
 }
 
